572646dc90eb4970a27a91d05079b2a72e0bd44a
Jason Thelen <jason.thelen@gmail.com>
2016-06-08 10:22:17 -0800

    add param parser
    allows passing objects as params, which will be converted to the format that the api expects
    allows passing of Date or similar objects, converted to formatted string expected by the api

--- lib/noaa_ncei_weather/weather.rb
+++ lib/noaa_ncei_weather/weather.rb
@@ -14,9 +14,28 @@ module NoaaNceiWeather
     end
 
     def self.where(endpoint, params = {})
+      params = self.parse_params(params)
       data = Connection.request(endpoint, params)['results']
       dslist = data.collect { |item| self.new(item) } if data && data.any?
       return dslist || []
+    end
+
+    def self.parse_params(params)
+      # Handle passing of weather objects as parameters for other api queries
+      objects = [:dataset, :datatype, :location, :station, :datacategory, :locationcategory]
+      objects.each do |object|
+        params[(object.to_s + "id").to_sym] = params.delete(object).id if params.has_key?(object)
+      end
+
+      # Handle Date, DateTime, or Time parameters
+      # Convert to formatted string the api is expecting
+      dates = [:startdate, :enddate]
+      dates.each do |date|
+        params[date] = params[date].iso8601 if params[date].respond_to?(:iso8601)
+      end
+
+      #return modified params
+      params
     end
   end
 end
